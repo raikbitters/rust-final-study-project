1f3cf0b081ea2145f108a1fca9571c637c55eddc
Raik Bitters <raikbitters@gmail.com>
2022-01-23 16:37:13 +0300

    Add thread pool

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,28 +1,42 @@
 use std::{
     net::{TcpListener, TcpStream}, 
     io::{Read, Write}, 
-    fs
+    fs,
+    thread, 
+    time::Duration,
 };
+
+use hello_web_service::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
     
+    println!("Shutting down.");
+
     fn handle_connection(mut stream: TcpStream) {
         let mut buffer = [0; 1024];
 
         stream.read(&mut buffer).unwrap();
 
-        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+        // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
         let get = b"GET / HTTP/1.1\r\n";
+        let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-        let (status_line, filename) = if buffer.starts_with(get) {
+        let (status_line, filename) = 
+        if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK", "hello.html")
+        } else if buffer.starts_with(sleep) {
+            thread::sleep(Duration::from_secs(5));
             ("HTTP/1.1 200 OK", "hello.html")
         } else {
             ("HTTP/1.1 404 NOT FOUND", "404.html")
